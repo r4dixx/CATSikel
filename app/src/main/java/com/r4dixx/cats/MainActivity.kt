4c351f8d58ac0161611cb6cf8d41e0a665cd1f2e
Amaël Sikel <amael.sikel@gmail.com>
2025-03-31 21:58:21 +0200

    Fix API call

--- app/src/main/java/com/r4dixx/cats/MainActivity.kt
+++ app/src/main/java/com/r4dixx/cats/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import com.r4dixx.cats.design.theme.CATSTheme
@@ -19,7 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             KoinContext {
                 CATSTheme {
-                    MasterScreen(Modifier.padding(spacingDefault))
+                    MasterScreen(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(spacingDefault)
+                    )
                 }
             }
         }
