8df70916589b61dba860cf71df80d82e2f36e11b
Amaël Sikel <amael.sikel@gmail.com>
2025-03-31 19:34:13 +0200

    Generate dependency graph

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,28 @@
+import dev.iurysouza.modulegraph.Theme
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.modulegraph) apply true
+}
+
+moduleGraphConfig {
+    readmePath.set("${rootDir}/docs/README.md")
+    heading.set("### Module Graph")
+    theme.set(
+        Theme.BASE(
+            mapOf(
+                "primaryTextColor" to "#fff",
+                "primaryColor" to "#5a4f7c",
+                "primaryBorderColor" to "#5a4f7c",
+                "lineColor" to "#f5a623",
+                "tertiaryColor" to "#40375c",
+                "fontSize" to "12px",
+            ),
+            focusColor = "#FA8140"
+        )
+    )
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,27 @@
 # CATS_Test_SIKEL
+
 CATS - Technical Test 2025
 
 Case study can be found in the repository: [docs/test_mobile_CA.pdf](test_mobile_CA.pdf)
+
+### Module Graph
+
+```mermaid
+%%{
+  init: {
+    'theme': 'base',
+    'themeVariables': {"primaryTextColor":"#fff","primaryColor":"#5a4f7c","primaryBorderColor":"#5a4f7c","lineColor":"#f5a623","tertiaryColor":"#40375c","fontSize":"12px"}
+  }
+}%%
+
+graph LR
+  :ui --> :core
+  :ui --> :domain
+  :app --> :core
+  :app --> :data
+  :app --> :domain
+  :app --> :ui
+  :data --> :core
+  :data --> :domain
+  :domain --> :core
+```
