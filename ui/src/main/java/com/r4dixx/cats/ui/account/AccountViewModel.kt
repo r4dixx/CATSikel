4792fd7d588daf9823668a5cc69ba196ffab8e18
Amaël Sikel <amael.sikel@gmail.com>
2025-04-01 21:31:20 +0200

    Sort operations by date

--- ui/src/main/java/com/r4dixx/cats/ui/account/AccountViewModel.kt
+++ ui/src/main/java/com/r4dixx/cats/ui/account/AccountViewModel.kt
@@ -5,6 +5,7 @@ import com.r4dixx.cats.core.utils.toFormattedAmount
 import com.r4dixx.cats.core.utils.toFormattedDate
 import com.r4dixx.cats.domain.model.Account
 import com.r4dixx.cats.domain.model.Operation
+import com.r4dixx.cats.ui.account.model.OperationUI
 import java.text.SimpleDateFormat
 import java.util.Locale
 import kotlin.time.ExperimentalTime
@@ -17,20 +18,22 @@ class AccountViewModel(account: Account, private val locale: Locale) : CATSViewM
         val newData = Data(
             label = account.label,
             balance = account.balance.toFormattedAmount(locale),
-            operations = account.operations.toOperationUIList()
+            operations = account.operations.toOperationsUI().sorted()
         )
         val newState = State.Success(newData)
         updateState(newState)
     }
 
     @OptIn(ExperimentalTime::class)
-    private fun List<Operation>.toOperationUIList() = map {
+    private fun List<Operation>.toOperationsUI() = map {
         OperationUI(
             title = it.title,
             amount = it.amount.toFormattedAmount(locale),
             date = it.date.toFormattedDate(SimpleDateFormat.SHORT, locale)
         )
     }
+
+    private fun List<OperationUI>.sorted() = sortedByDescending { it.date }
 
     data class Data(
         val label: String = "",
--- ui/src/main/java/com/r4dixx/cats/ui/account/model/OperationUI.kt
+++ ui/src/main/java/com/r4dixx/cats/ui/account/model/OperationUI.kt
@@ -1,4 +1,4 @@
-package com.r4dixx.cats.ui.account
+package com.r4dixx.cats.ui.account.model
 
 data class OperationUI(
     val title: String,
