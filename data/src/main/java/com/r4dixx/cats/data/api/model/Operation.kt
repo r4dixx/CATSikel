3be8eadb3b987bb84b06b0471f88663ace9555b9
Amaël Sikel <amael.sikel@gmail.com>
2025-04-01 13:27:03 +0200

    Serialize date to instant

--- data/src/main/java/com/r4dixx/cats/data/api/model/Operation.kt
+++ data/src/main/java/com/r4dixx/cats/data/api/model/Operation.kt
@@ -1,12 +1,17 @@
 package com.r4dixx.cats.data.api.model
 
+import com.r4dixx.cats.data.api.serializer.InstantSerializer
 import kotlinx.serialization.Serializable
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
+@OptIn(ExperimentalTime::class)
 @Serializable
 data class Operation(
     val id: Long,
     val title: String,
     val amount: String,
     val category: String,
-    val date: Long
+    @Serializable(with = InstantSerializer::class)
+    val date: Instant
 )
