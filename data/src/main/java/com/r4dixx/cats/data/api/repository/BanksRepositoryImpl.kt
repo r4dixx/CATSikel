a09f20d91bc97033473edaad520c4bd645055149
Amaël Sikel <amael.sikel@gmail.com>
2025-04-02 14:13:24 +0200

    Enable orientation lock for activity

--- data/src/main/java/com/r4dixx/cats/data/api/repository/BanksRepositoryImpl.kt
+++ data/src/main/java/com/r4dixx/cats/data/api/repository/BanksRepositoryImpl.kt
@@ -5,7 +5,7 @@ import com.r4dixx.cats.data.api.service.BanksService
 import com.r4dixx.cats.domain.model.Bank
 import com.r4dixx.cats.domain.repository.BanksRepository
 
-// TODO get API if API fails get local file
+// TODO - get API if API fails get local file
 class BanksRepositoryImpl(private val service: BanksService) : BanksRepository {
     override fun getBanks(): Result<List<Bank>> {
         return service.getResult().mapCatching { response ->
