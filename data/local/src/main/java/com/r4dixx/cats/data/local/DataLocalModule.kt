d3c04f2b224a2f8a1b3b873932a53edddced141b
Amaël Sikel <amael.sikel@gmail.com>
2025-04-21 11:40:27 +0200

    Fix DI

--- data/local/src/main/java/com/r4dixx/cats/data/local/DataLocalModule.kt
+++ data/local/src/main/java/com/r4dixx/cats/data/local/DataLocalModule.kt
@@ -7,7 +7,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataLocalModule = module {
-    single { get<CATSDatabase>().bankDao }
+    single { get<CATSDatabase>() }
     single { BanksLocalDataSource(get()) }
     single {
         Room.databaseBuilder(
