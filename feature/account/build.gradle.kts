d91c75603227c0efda63ad8b904c29e31275c1e4
Amaël Sikel <amael.sikel@gmail.com>
2025-04-22 21:00:54 +0200

    Fix wrong declaration

--- feature/account/build.gradle.kts
+++ feature/account/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(libs.koin.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
-    debugImplementation(libs.bundles.compose.debug)
+    implementation(libs.bundles.compose.debug)
     lintChecks(libs.bundles.compose.lint)
 
     implementation(libs.kotlinx.collections.immutable)
