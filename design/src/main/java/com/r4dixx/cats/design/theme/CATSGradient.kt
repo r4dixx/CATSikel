0c0945b6aa151b10722fb482bf3f2bdfb9bd8b32
Amaël Sikel <amael.sikel@gmail.com>
2025-04-22 16:26:28 +0200

    Simplify CATSGradient

--- design/src/main/java/com/r4dixx/cats/design/theme/CATSGradient.kt
+++ design/src/main/java/com/r4dixx/cats/design/theme/CATSGradient.kt
@@ -3,7 +3,6 @@ package com.r4dixx.cats.design.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 
 object CATSGradient {
@@ -14,8 +13,6 @@ object CATSGradient {
             colors = listOf(
                 MaterialTheme.colorScheme.primary,
                 MaterialTheme.colorScheme.secondary,
-            ),
-            start = Offset(0f, Float.POSITIVE_INFINITY),
-            end = Offset(Float.POSITIVE_INFINITY, 0f)
+            )
         )
 }
