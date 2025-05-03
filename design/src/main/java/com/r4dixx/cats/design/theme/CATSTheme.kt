a8002b22038925dc28f7202aaf88efe887f3023c
Amaël Sikel <amael.sikel@gmail.com>
2025-05-03 18:47:48 +0200

    Fix gradient support

--- design/src/main/java/com/r4dixx/cats/design/theme/CATSTheme.kt
+++ design/src/main/java/com/r4dixx/cats/design/theme/CATSTheme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
@@ -38,9 +39,13 @@ fun CATSTheme(
         else -> lightColorScheme()
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = CATSTypography.default,
-        content = content
-    )
+    val gradient = CATSGradient(enabled = darkGradientEnabled)
+
+    CompositionLocalProvider(LocalCATSGradient provides gradient) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = CATSTypography.default,
+            content = content
+        )
+    }
 }
